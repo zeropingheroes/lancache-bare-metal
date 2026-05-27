a8e4c65bb2342d867a1e3bc2543ad37936792df4
ilumos <ilumos@gmail.com>
2026-05-27 18:28:20 +0000

    When clearing cache, stop and start filebeat

--- clear-cache.sh
+++ clear-cache.sh
@@ -3,8 +3,10 @@
 set -e
 
 systemctl stop nginx
+systemctl stop filebeat
 
 rm -rf /srv/lancache/data/*
 rm -rf /srv/lancache/logs/*.log /srv/lancache/logs/http/*.log /srv/lancache/logs/stream/*.log
 
 systemctl start nginx
+systemctl start filebeat
